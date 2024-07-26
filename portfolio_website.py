29b5616b0066d21f575946a561e51a9fa1f8e5d0
Furkan Ayakdaş <72611180+furkan0920@users.noreply.github.com>
2024-07-26 22:41:00 +0300

    Update portfolio_website.py

--- portfolio_website.py
+++ portfolio_website.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import google.generativeai as genai
 
-
-genai.configure(api_key="AIzaSyCKcUfHsQNbuxHKwnekPfVWkQczmetytaE")
+api_key=st.secrets["GOOGLE_API_KEY"]
+genai.configure(api_key=api_key)
 
 model = genai.GenerativeModel('gemini-1.5-flash')
 
